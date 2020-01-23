0692eeb6c6d2202e986f3e2af4daaa8abf1e7be0
Mohi7-Agg <mohit.aggbsr@gmail.com>
2020-01-24 01:07:16 +0530

    gitignore fix

---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+Steps to run :
+
+Pass valid file path as cmd arguments.
+
+Eg : ".\input.txt"
---
+++ SubmissionProvider/Exceptions/InvalidSourceFormatException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubmissionProvider.Exceptions
+{
+    public class InvalidSourceFormatException : Exception
+    {
+        public InvalidSourceFormatException(string msg)
+        {
+            throw new Exception(msg);
+        }        
+    }
+}
